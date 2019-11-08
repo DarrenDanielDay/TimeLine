422dae44306f89c7c577ec0b8e28935645c946a0
DarrenDanielDay <Darren_Daniel_Day@hotmail.com>
2019-11-08 16:14:58 +0800

    添加前端初始配置

---
+++ timeline-ui/src/components/TimeLine.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="timeline-app">hello,ts!{{msg}}</div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "TimeLine",
+  methods: {},
+  data() {
+    return {
+      msg: "i'm message"
+    };
+  },
+});
+</script>
+
+<style scoped>
+#timeline-app {
+  background-color: aqua;
+}
+</style>
