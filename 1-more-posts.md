e680be4bf3aca1953b086bc63bf630894dd2ddff
Lu Wen Qing <david1113531523@gmail.com>
2019-11-13 20:59:10 +0800

    明确api接口，添加api说明文档

---
+++ 1-more-posts.md
@@ -0,0 +1,79 @@
+## 获取更多POST
+
+---
+
+### 功能描述
+
+初次加载返回特定（5条）条数的POST、或者获取某时间点之后的所有的POST（时间倒序，最新发布的POST在最前面）
+
+### 请求地址
+
+```POST https://ecnuer996.cn/timeline/more-posts```
+
+### 请求参数
+
+属性      |   类型 | 说明  
+:----------:|:---------:|:---------:
+time   | string  | 可选，格式为“yyyy-MM-dd hh:mm:ss”<br>未设置将返回最新的五条POST，<br>设置后将返回指定时间之前的所有POST
+
+### 返回值
+
+**Object**
+返回的JSON数据包
+
+属性       | 类型       | 说明  
+:----------:|:---------:|:---------: 
+posts     | array     | POST列表
+
+数组posts中元素的属性
+
+属性       | 类型       | 说明  
+:----------:|:---------:|:---------: 
+nickname     | string     | 用户名
+avatar | string | 用户头像URL
+postId | number | POST的ID
+postTime | string | POST发布时间
+content | string | POST内容
+images | array | POST附加的图片列表
+
+数组images中每个元素为一个字符串，是对应图片的URL
+
+## 刷新操作
+
+---
+
+### 功能描述
+
+点击刷新操作，后台随机生成若干POST并返回至多十条的最新POST
+
+### 请求地址
+
+```POST https://ecnuer996.cn/timeline/refresh```
+
+### 请求参数
+
+属性      |   类型 | 说明  
+:----------:|:---------:|:---------:
+time   | string  | 前端列表中最新POST的发布时间，格式为“yyyy-MM-dd hh:mm:ss”
+
+### 返回值
+
+**Object**
+返回的JSON数据包
+
+属性       | 类型       | 说明  
+:----------:|:---------:|:---------: 
+posts     | array     | POST列表
+
+数组posts中元素的属性
+
+属性       | 类型       | 说明  
+:----------:|:---------:|:---------: 
+nickname     | string     | 用户名
+avatar | string | 用户头像URL
+postId | number | POST的ID
+postTime | string | POST发布时间
+content | string | POST内容
+images | array | POST附加的图片列表
+
+数组images中每个元素为一个字符串，是对应图片的URL
--- src/main/java/cn/ecnuer996/timeline/controller/UserController.java
+++ src/main/java/cn/ecnuer996/timeline/controller/UserController.java
@@ -3,14 +3,16 @@ package cn.ecnuer996.timeline.controller;
 import cn.ecnuer996.timeline.bean.Post;
 import cn.ecnuer996.timeline.bean.User;
 import cn.ecnuer996.timeline.dao.PostDao;
+import cn.ecnuer996.timeline.dao.PostImageDao;
 import cn.ecnuer996.timeline.dao.UserDao;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.List;
 
 @CrossOrigin
@@ -20,6 +22,10 @@ public class UserController {
     private UserDao userDao;
     @Autowired
     private PostDao postDao;
+    @Autowired
+    private PostImageDao postImageDao;
+
+    private SimpleDateFormat dateFormat=new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
 
     @RequestMapping(value="/all-user",method= RequestMethod.GET)
     public List<User> getAllUser(){
@@ -36,9 +42,37 @@ public class UserController {
         int id = Integer.parseInt(request.getParameter("id"));
         return postDao.listNextFivePost(id);
     }
+
     @RequestMapping(value = "/previous-five-post",method = RequestMethod.GET)
     public List<Post> getPreviousFivePost(HttpServletRequest request) {
         int id = Integer.parseInt(request.getParameter("id"));
         return postDao.listPreviousFivePost(id);
+    }
+
+    @RequestMapping(value = "more-posts",method = RequestMethod.GET)
+    public JSONObject getMorePosts(@RequestParam(required = false) Integer id){
+        JSONObject response=new JSONObject();
+        JSONArray items=new JSONArray();
+        List<Post> posts;
+        if(id!=null){
+            posts=postDao.listNextFivePost(id);
+        }else{
+            posts=postDao.listLatestFivePost();
+        }
+        for(int i=0;i<posts.size();++i){
+            JSONObject item=new JSONObject();
+            Post post=posts.get(i);
+            User user=userDao.selectUserById(post.getUserId());
+            List<String> images=postImageDao.selectImagesByPostId(post.getId());
+            item.put("nickname",user.getNickname());
+            item.put("avatar",user.getAvatar());
+            item.put("id",post.getId());
+            item.put("content",post.getContent());
+            item.put("postTime",dateFormat.format(post.getTime()));
+            item.put("images",images);
+            items.add(item);
+        }
+        response.put("posts",items);
+        return response;
     }
 }
